d694043da695df7a1eeb712430a008222053e8d0
waleedzaman41 <waleedzaman41@gmail.com>
2022-06-23 17:13:25 -0700

    All code

---
+++ backend/data.js
@@ -0,0 +1,73 @@
+import bcrypt from 'bcryptjs';
+const data = {
+  users: [
+    {
+      name: 'Waseh',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: '1',
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 3,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      // _id: '2',
+      name: 'Adidas fit shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 0,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      // _id: '3',
+      name: 'Nike Slim pant',
+      slug: 'nike-slim-pant',
+      category: 'pant',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pant',
+    },
+    {
+      // _id: '4',
+      name: 'Adidas fit pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality Product',
+    },
+  ],
+};
+
+export default data;
