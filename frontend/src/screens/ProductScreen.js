c75766975d8d40a5fec719be4bf8b1a876b8a5c7
waleedzaman41 <waleedzaman41@gmail.com>
2022-07-03 15:00:47 -0700

    product screen changes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -147,7 +147,7 @@ function ProductScreen() {
             </ListGroup.Item>
             <ListGroup.Item>Price : Rs. {product.price}</ListGroup.Item>
             <ListGroup.Item>
-              <Row xs={1} md={2} className="g-2">
+              <Row xs={3} md={3} className="g-2">
                 {[product.image, ...product.images].map((x) => (
                   <Col key={x}>
                     <Card>
