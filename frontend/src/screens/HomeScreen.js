d40e42193cff6a15dd7bfd8e4a827af481ceead9
waleedzaman41 <waleedzaman41@gmail.com>
2022-07-03 14:40:43 -0700

    product size change for mobile view

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -59,7 +59,14 @@ function HomeScreen() {
         ) : (
           <Row>
             {products.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+              <Col
+                key={product.slug}
+                sm={6}
+                md={4}
+                lg={3}
+                xs={6}
+                className="mb-3"
+              >
                 <Product product={product}></Product>
               </Col>
             ))}
